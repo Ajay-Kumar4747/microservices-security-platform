27d210d9e8b090ab48ddc9d5d3790dfe6fb89246
Ajay-Kumar4747 <ak9666770821@gmail.com>
2026-06-13 02:22:16 +0530

    updated config server

---
+++ security-config-server/src/test/java/com/ajay/security_config_server/SecurityConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.security_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
